7db27209a326815473c8ebc47d9ed7afa8335f87
Kevin Fox <kevin@efox.cc>
2016-09-02 16:46:50 -0700

    Add a way to block until a file shows up. Kill the container when asked properly.

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -e
-
+(
+if [ "x$START_WAIT_FILE" != "x" ]; then
+	while true; do
+		[ -f "$START_WAIT_FILE" ] && break
+		sleep 1
+	done
+fi
 /usr/sbin/nslcd -d
+) & pid=$!
+trap "kill $pid" TERM
+wait $pid
